945cff6c03aea604c1323f1a5bbf3465adf014ae
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2023-02-02 18:47:30 -0300

    Added basic wireframe

---
+++ App.tsx
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import type { PropsWithChildren } from "react";
+import {
+	FlatList,
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	View,
+	useColorScheme,
+} from "react-native";
+
+import CryptoList from "./src/components/CryptoList";
+
+const Stack = createNativeStackNavigator();
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+	Colors,
+	DebugInstructions,
+	Header,
+	LearnMoreLinks,
+	ReloadInstructions,
+} from "react-native/Libraries/NewAppScreen";
+
+type SectionProps = PropsWithChildren<{
+	title: string;
+}>;
+
+function App(): JSX.Element {
+	const isDarkMode = useColorScheme() === "dark";
+
+	const backgroundStyle = {
+		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+	};
+
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="List" component={CryptoList} options={{ headerShown: false }} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+
+export default App;
---
+++ src/components/CryptoList.tsx
@@ -0,0 +1,135 @@
+import CoinData from "../types/CoinData";
+import React from "react";
+import { FlatList, Image, Text, View } from "react-native";
+import styled from "styled-components";
+
+const cryptos: CoinData[] = [
+	{
+		id: "1",
+		name: "Bitcoin",
+		symbol: "BTC",
+		price: 7215.68,
+		percent: 1.82,
+		img: require("../assets/img/bitcoin.png"),
+	},
+	{
+		id: "2",
+		name: "Ethereum",
+		symbol: "ETH",
+		price: 146.83,
+		percent: 1.46,
+		img: require("../assets/img/ethereum.png"),
+	},
+	{
+		id: "3",
+		name: "XRP",
+		symbol: "XRP",
+		price: 0.220568,
+		percent: -2.47,
+		img: require("../assets/img/xrp.png"),
+	},
+];
+
+const TopBar = styled(View)`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px;
+  padding-top: 40px;
+  height: 130px;
+  background-color: rgb(51,89,118);
+  border-bottom: 1px solid black;
+`;
+const Title = styled(Text)`
+  font-size: 20px;
+  font-weight: bold;
+  color: white;
+`;
+
+export default function CryptoList() {
+	return (
+		<>
+			<TopBar>
+				<Title>CryptoTracker Pro</Title>
+				<Image source={require("../assets/img/avatar.png")} />
+			</TopBar>
+			<FlatList
+				data={cryptos}
+				renderItem={({ item }) => <List item={item} />}
+				keyExtractor={(item) => item.id}
+			/>
+		</>
+	);
+}
+const CryptoContainer = styled(View)`
+    flex-direction: row;
+    justify-content: space-between;
+		align-items: center;
+		margin: 0px 24px;
+    padding: 24px 0px;
+    border-bottom-width: 1px;
+    border-color: #e5e5e5;
+`;
+
+const CryptoInfo = styled(View)`
+    flex-direction: row;
+    align-items: center;
+`;
+
+const CryptoText = styled(View)`
+    margin-left: 10px;
+`;
+
+const CryptoName = styled(Text)`
+    font-size: 16px;
+    font-weight: bold;
+`;
+
+const CryptoSymbol = styled(Text)`
+    font-size: 14px;
+    color: #434343;
+`;
+
+const CryptoPrice = styled(View)`
+    align-items: flex-end;
+`;
+
+const CryptoPriceText = styled(Text)`
+    font-size: 16px;
+    font-weight: 600;
+    margin-bottom: 6px;
+`;
+
+const CryptoPercent = styled(Text)<{ percent: number }>`
+    color:${(props) => (props.percent > 0 ? "rgb(0,115,59)" : "#DD2C00")};
+`;
+const PercentArrow = styled(View)`
+    align-items: center;
+`;
+
+function List({ item }: { item: CoinData }) {
+	return (
+		<CryptoContainer>
+			<CryptoInfo>
+				<Image source={item.img} />
+				<CryptoText>
+					<CryptoName>{item.name}</CryptoName>
+					<CryptoSymbol>{item.symbol}</CryptoSymbol>
+				</CryptoText>
+			</CryptoInfo>
+			<CryptoPrice>
+				<CryptoPriceText>${item.price}</CryptoPriceText>
+				<CryptoPercent percent={item.percent}>
+					<PercentArrow>
+						{item.percent > 0 ? (
+							<Image source={require("../assets/img/arrow-up.png")} />
+						) : (
+							<Image source={require("../assets/img/arrow-down.png")} />
+						)}
+					</PercentArrow>
+					<Text>{item.percent}%</Text>
+				</CryptoPercent>
+			</CryptoPrice>
+		</CryptoContainer>
+	);
+}
